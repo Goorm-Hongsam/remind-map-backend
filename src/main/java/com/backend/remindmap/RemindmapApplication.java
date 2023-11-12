b8aaa950fcaf25e67cdc63c81f8fda5fbe81cd5d
woo <ldw28517@naver.com>
2023-11-12 16:26:01 +0900

    init project

---
+++ src/main/java/com/backend/remindmap/RemindmapApplication.java
@@ -0,0 +1,13 @@
+package com.backend.remindmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindmapApplication.class, args);
+	}
+
+}
