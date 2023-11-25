8d81b74f3614355be1ff492284845fac88bae7b7
dbstn1325 <dbstn1325@naver.com>
2023-11-26 00:29:15 +0900

    fix: @Valid 어노테이션 수정

--- src/main/java/com/backend/remindmap/marker/dto/request/MarkerLocationRequest.java
+++ src/main/java/com/backend/remindmap/marker/dto/request/MarkerLocationRequest.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @Getter
 public class MarkerLocationRequest {
-    @NotBlank(message = "공백일 수는 없습니다.")
+    @NotNull(message = "공백일 수는 없습니다.")
     private Double latitude;
 
-    @NotBlank(message = "공백일 수는 없습니다.")
+    @NotNull(message = "공백일 수는 없습니다.")
     private Double longitude;
 }
